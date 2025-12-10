39ab381c917cbeb4faf0930fb4b8dc9c375e6756
24DP1JBorh <a242182jb@rvt.lv>
2025-12-10 20:29:46 +0200

    Advanced astrology (3 parts)

--- src/main/java/rvt/AdvancedAstrology.java
+++ src/main/java/rvt/AdvancedAstrology.java
@@ -25,7 +25,23 @@ public class AdvancedAstrology {
         }
     }
 
+    public static void christmasTree(int height) {
+        for (int i = 0; i < height; i++) {
+            printSpaces(height - i - 1);
+            printStars(2 * i + 1);
+            System.out.println();
+        }
+        for (int i = 0; i < 2; i++) {
+            printSpaces(height - 2);
+            printStars(3);
+            System.out.println();
+        }
+    }
+
     public static void main(String[] args) {
         printTriangle(4);
+        christmasTree(10);
+        christmasTree(4);
+
     }
 }
