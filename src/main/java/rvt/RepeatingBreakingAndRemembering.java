88ed668116cd0702fed52a8bac1d6335276c8b79
24DP1JBorh <a242182jb@rvt.lv>
2025-12-10 17:20:18 +0200

    Repeating, breaking and remembering(1 part)

---
+++ src/main/java/rvt/RepeatingBreakingAndRemembering.java
@@ -0,0 +1,21 @@
+package rvt;
+import java.util.Scanner;
+public class RepeatingBreakingAndRemembering {
+    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("Give numbers:");
+
+        while (true) {
+            int num = Integer.valueOf(scanner.nextLine());
+
+            if (num == -1) {
+                break;
+            }
+
+        }
+
+        System.out.println("Thx! Bye!");
+    }
+}
