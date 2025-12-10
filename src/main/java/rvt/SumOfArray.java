1d5621620d4931a274e8d7ae29a02733c7ef8ea7
24DP1JBorh <a242182jb@rvt.lv>
2025-12-10 22:11:49 +0200

    Sum of array

---
+++ src/main/java/rvt/SumOfArray.java
@@ -0,0 +1,20 @@
+package rvt;
+
+public class SumOfArray {
+    
+    public static void main(String[] args) {
+        int[] numbers = {5, 1, 3, 4, 2};
+        sumOfNumbersInArray(numbers);
+    }
+
+
+    public static int sumOfNumbersInArray(int[] array) {
+        
+        int sum = 0;
+        for (int index = 0; index < array.length; index++) {
+            sum += array[index];
+        }
+
+        return sum;
+    }
+}
