3831d35e76ac1945a09cf182b8a85de7f007024e
24DP1JBorh <a242182jb@rvt.lv>
2025-12-10 19:50:42 +0200

    Advanced astrology(1 part)

---
+++ src/main/java/rvt/AdvancedAstrology.java
@@ -0,0 +1,31 @@
+package rvt;
+
+public class AdvancedAstrology {
+    
+    public static void printSpaces(int number) {
+        
+        for (int i = 0; i < number; i++) {
+            System.out.print(" ");
+        }    
+
+    }
+
+    public static void printStars(int number) {
+
+        for (int i = 0; i < number; i++) {
+            System.out.print("*");
+        }
+    }
+
+    public static void printTriangle(int size) {
+        for (int i = 0; i < size; i++) {
+            printSpaces(size - i - 1);
+            printStars(i + 1);
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        printTriangle(4);
+    }
+}
