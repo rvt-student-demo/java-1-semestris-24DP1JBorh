13e6334b162da12bc0e117c413299ee29e39f08b
24DP1JBorh <a242182jb@rvt.lv>
2025-12-10 17:53:05 +0200

    Repeating, breaking and remembering(5 parts)

--- src/main/java/rvt/RepeatingBreakingAndRemembering.java
+++ src/main/java/rvt/RepeatingBreakingAndRemembering.java
@@ -7,6 +7,8 @@ public class RepeatingBreakingAndRemembering {
         
         int sum = 0;
         int counter = 0;
+        int even = 0;
+        int odd = 0;
 
         System.out.println("Give numbers:");
 
@@ -19,11 +21,21 @@ public class RepeatingBreakingAndRemembering {
 
             sum += num;
             counter++;
+
+            if (num % 2 == 0) {
+                even++;
+            } else {
+                odd++;
+            }
         }
 
         System.out.println("Thx! Bye!");
         System.out.println("Sum: " + sum);
         System.out.println("Numbers: " + counter);
         System.out.println("Average: " + ((double)sum / counter));
+        System.out.println("Even: " + even);
+        System.out.println("Odd: " + odd);
+
+        scanner.close();
     }
 }
